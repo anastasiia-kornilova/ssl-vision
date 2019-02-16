c37b5e1d98e3847878180fb4cf25a694b07d2806
Dmitriy Jarosh <dimajarosh@gmail.com>
2019-02-16 20:19:24 +0300

    Update InstallPackagesUbuntu.sh

--- InstallPackagesUbuntu.sh
+++ InstallPackagesUbuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # These are the packages required for install on Ubuntu systems.
-sudo apt-get install g++ libqt5-dev libeigen3-dev protobuf-compiler libprotobuf-dev libdc1394-22 libdc1394-22-dev cmake libv4l-0
+sudo apt-get install g++ qt5-default libeigen3-dev protobuf-compiler libprotobuf-dev libdc1394-22 libdc1394-22-dev cmake libv4l-0
